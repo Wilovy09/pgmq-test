53da9e5611b70706ab15c37323959246fa1fdf45
wilovy <computadora@Computadoras-MacBook-Pro.local>
2026-01-27 01:01:35 -0600

    feat: :sparkles: Add `::default()` to Config

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,13 @@ pub struct Config {
 }
 
 impl Config {
+    #[allow(dead_code)]
+    pub fn default() -> Self {
+        Config {
+            database_url: "postgresql://postgres:password@localhost:5432/pgmq".to_string(),
+        }
+    }
+
     pub fn from_env() -> Self {
         let database_url = std::env::var("DATABASE_URL")
             .expect("DATABASE_URL must be set in environment variables");
